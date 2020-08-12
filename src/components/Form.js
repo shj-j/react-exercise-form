e7437dd0cce241c13d3d2b57266e7c5a2119e52f
shj-j <shang.jie@outlook.com>
2020-08-12 22:17:31 +0800

    [sj]: add css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import './myProfile.less';
 
 class Form extends Component{
     state={
-        name: "Your name",
-        gender:"Female",
-        description:"Description about yourself",
+        name: "",
+        gender:"",
+        description:"",
         checkbox: "",
     }
     handleSubmit=(event)=>{
@@ -33,28 +34,26 @@ class Form extends Component{
     }
     render(){
         return(
-            <form onSubmit={this.handleSubmit}>
-                <h1>My Profile</h1>
-                <div>
-                    <h3>Name</h3>
-                    <input htmlFor="name" type="text" onChange={this.handleNameChange} value = {this.state.name}/>
-                </div>
-                <div>
-                    <h3>Gender</h3>
-                    <select value={this.state.gender} onChange= {this.handleGender} >
+            <form onSubmit={this.handleSubmit} className="form">
+                <h1 className="title">My Profile</h1>
+                <div className="form-content"> 
+                    <h3 className="lable_title">Name</h3>
+                    <input className="input_text" type="text" onChange={this.handleNameChange} value = {this.state.name} placeholder="Your name"/>
+                    <h3 className="lable_title">Gender</h3>
+                    <select className="input_text" value={this.state.gender} onChange= {this.handleGender} placeholder="Female" >
                         <option value="Female">Female</option>
                         <option value="Male">Male</option>
                     </select>
+                    <h3 className="lable_title">Description</h3>
+                    <textarea value={this.state.description}
+                    placeholder="Description about yourself" onChange={this.handleDescription} />
+
+                    <div className="agreement-div">
+                        <input type="checkbox" onChange={this.handleCheckbox}/>
+                        <p className="agreement-p"> I have read the terms of conduct</p>
+                    </div>
                 </div>
-                <div>
-                    <h3>Description</h3>
-                    <textarea value={this.state.description} onChange={this.handleDescription} />
-                </div>
-                <div style={{display:'flex', alignItems:'center'}}>
-                    <input type="checkbox" onChange={this.handleCheckbox}/>
-                    <p> I have read the terms of conduct</p>
-                </div>
-                <input type="submit" value="submit"/>
+                <input className="submit" type="submit" value="Submit"/>
             </form>
         );
     }
